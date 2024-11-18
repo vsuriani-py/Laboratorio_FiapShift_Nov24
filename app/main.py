69630f06d7fa4b1c20088c340e2c1b0d24b9eef2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-18 20:57:07 -0300

    Atualização main n

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Hello World"}
+    return {"Hello": "Mundo melius quid fecistiS"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
